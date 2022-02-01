10f28e9c7de67ecc8613117e568789f457b3dfbc
paradpgxlex <trashmailvomSommerschein@openrobotx.de>
2022-02-01 12:15:52 +0100

    Cleanup

--- Frontend/src/components/Landing/index.jsx
+++ Frontend/src/components/Landing/index.jsx
@@ -20,53 +20,45 @@ function Landing(props) {
   return (
     <div className="container-center-horizontal">
       <div className="landing screen">
-        <div className="overlap-group4">
+        <div className="overlap-group-logo">
           <div className="banner">
-            <div className="banner_-text">{banner_Text}</div>
+            <div className="banner-text">{banner_Text}</div>
           </div>
-          <div className="overlap-group">
-            <div className="background_-image"></div>
+          <div className="overlap-group-image">
+            <div className="background-image"></div>
             <img className="logo" src={logo} />
           </div>
         </div>
         <div className="overlap-group-container">
-          <div className="overlap-group3">
+          <div className="overlap-group-content">
             <div className="about">
-              <h1 className="about_-text montserrat-semi-bold-white-50px">{about_Text}</h1>
+              <h1 className="about-titel montserrat-semi-bold-white-50px">{about_Text}</h1>
             </div>
             <div className="about-text">
-              <div className="about_-icon">
-                <div className="ebene-1">
-                  <div className="pfad-container">
-                    <img className="pfad-5" src={pfad5} />
-                  </div>
-                </div>
-              </div>
-              <div className="overlap-group1">
-                <div className="about-text_-text montserrat-normal-black-25px">{abouttext_Text}</div>
+              <img className="about-icon" src={pfad5} />
+              <div className="overlap-group-about">
+                <div className="about-text-text montserrat-normal-black-25px">{abouttext_Text}</div>
               </div>
             </div>
             <div className="project">
-              <div className="project_-icon">
-                <div className="ebene-1-1">
-                  <img className="pfad-4" src={pfad4} />
-                </div>
+              <div className="project-icon">
+                <img className="project-icon-icon" src={pfad4} />
               </div>
-              <div className="project_-text montserrat-semi-bold-white-50px">{project_Text}</div>
+              <div className="project-titel montserrat-semi-bold-white-50px">{project_Text}</div>
             </div>
           </div>
-          <div className="overlap-group5">
+          <div className="overlap-group-project">
             <div className="project-text">
-              <div className="project-text_-text montserrat-normal-black-25px">{projecttext_Text}</div>
+              <div className="project-text-text montserrat-normal-black-25px">{projecttext_Text}</div>
             </div>
             <div className="cta">
-              <div className="cta_-text1 montserrat-normal-black-35px">{cta_Text1}</div>
-              <div className="cta_-button">
-                <div className="cta_-button_-text">{cta_Button_Text}</div>
+              <div className="cta-titel montserrat-normal-black-35px">{cta_Text1}</div>
+              <div className="cta-button">
+                <div className="cta-button-text">{cta_Button_Text}</div>
               </div>
-              <div className="cta_-container montserrat-normal-black-35px">
-                <div className="cta_-text2">{cta_Text2}</div>
-                <div className="cta_mail">{cta_Mail}</div>
+              <div className="cta-container montserrat-normal-black-35px">
+                <div className="cta-text">{cta_Text2}</div>
+                <div className="cta-mail">{cta_Mail}</div>
               </div>
             </div>
           </div>
