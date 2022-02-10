fefbcc69dfd19f4198d3e639567a1413e54f9307
= <eddie@make-europe.com>
2022-02-10 16:07:54 -0700

    cleanup

--- Backend/imagery.py
+++ Backend/imagery.py
@@ -9,8 +9,8 @@ def insertRandomImage(id, table):
     img_size = (128,128)
     img = get_random_image(img_size)
     name = str(uuid.uuid4())
-    matplotlib.image.imsave("test/" + name + ".png", img)
-    path = os.getcwd()+'/test/'+name+'.png'
+    matplotlib.image.imsave(name + ".png", img)
+    path = os.getcwd() + name + '.png'
     encoded = base64.b64encode(open(path, "rb").read())
     string = 'data:image/png;base64,{}'.format(encoded)
     dbEntry = db.session.query(table).get(id)
