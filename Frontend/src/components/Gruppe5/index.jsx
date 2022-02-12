4309e12c5c7eb52d46a3718c8f418b0f714bae1a
paradpgxlex <trashmailvomSommerschein@openrobotx.de>
2022-02-12 14:34:20 -0700

    Refine

--- Frontend/src/components/Gruppe5/index.jsx
+++ Frontend/src/components/Gruppe5/index.jsx
@@ -6,23 +6,29 @@ function Gruppe5(props) {
   const { className, event } = props;
 
   return (
-    <div className={`gruppe-5-1 border-1px-dove-gray ${className || ""}`}>
-      <img className="event_-picture" src={event.image} />
+    <div className={`gruppe-5-1 ${className || ""}`}>
+      <img className="event_-picture border-1px-dove-gray" src={event.image} />
       <div className="flex-col">
         <div className="flex-row-1">
           <div className="event_-container">
-            <div className="event_-host montserrat-medium-black-30px">{event.hostName}</div>
+            <div className="event_-host montserrat-medium-black-30px">{event.name.length >= 35 ? event.name.substring(0, 35) + " ..." : event.name}</div>
             <div className="event_-location montserrat-medium-black-20px">{event.city}</div>
           </div>
-          <div className="event_-eventname montserrat-normal-black-30px">{event.name}</div>
         </div>
         <div className="event_-container-1 montserrat-medium-black-30px">
           <div className="event_-date">{event.date}</div>
           <div className="event_-time">{event.start} - {event.end}</div>
         </div>
-        <div className="event_-lineup montserrat-medium-black-20px">{event.lineup}</div>
+        <div className="event_-lineup montserrat-medium-black-20px">{event.lineup.length >= 50 ? event.lineup.substring(0, 50) + " ..." : event.lineup}</div>
       </div>
-      <Link to="/eventdetails">
+      <Link
+        to={{
+          pathname: "/eventdetails",
+          state: {
+            event: (event),
+          },
+        }}
+      >
         <div className="overlap-group border-1px-dove-gray">
           <div className="more_-text montserrat-medium-black-30px">
             Find
