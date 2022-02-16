7e80df8a635d439d636b597fbc74ea629dbb50b2
= <eddie@make-europe.com>
2022-02-16 12:09:37 -0700

    isso

--- Backend/app.py
+++ Backend/app.py
@@ -9,79 +9,54 @@ from flask_marshmallow import Marshmallow
 from services import (Relation_EventHost_Service, Relation_EventTicket_Service, UserService, UserListService, HostService, 
                     EventService, HostListService, EventListService, Relation_UserHost_Service, CountService, CountListService, EventImageService, 
                     CommentService, CommentListService, ListCommentsByEventService, TicketService, TicketCreateService)
-
-#*______________ App Setup ______________
-app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#*______________ DB Setup ______________
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///acceth.db'
-db = SQLAlchemy(app)
-
-#*______________ Marshmallow Setup ______________
-ma = Marshmallow(app)
-
-#*______________ API & Swagger Setup ______________
-api = Api(app)
-app.config.update({
-    'APISPEC_SPEC': APISpec(
-        title='Acceth Backend',
-        version='v0.0.1',
-        plugins=[MarshmallowPlugin()],
-        openapi_version='2.0.0'
-    ),
-    'APISPEC_SWAGGER_URL': '/swagger/',
-    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'
-})
-
-#*______________ Docs Setup ______________
-docs = FlaskApiSpec(app)
+from config import app, api, docs
 
 
 
 #*______________ Service Registration ______________
-api.add_resource(UserService, '/user/<user_id>')
+api.add_resource(UserService, '/api/user/<user_id>')
 docs.register(UserService)
-api.add_resource(UserListService, '/list/user')
+api.add_resource(UserListService, '/api/list/user')
 docs.register(UserListService)
 
-api.add_resource(EventService, '/event/<event_id>')
+api.add_resource(EventService, '/api/event/<event_id>')
 docs.register(EventService)
-api.add_resource(EventListService, '/list/event')
+api.add_resource(EventListService, '/api/list/event')
 docs.register(EventListService)
 
-api.add_resource(HostService, '/host/<host_id>')
+api.add_resource(HostService, '/api/host/<host_id>')
 docs.register(HostService)
-api.add_resource(HostListService, '/list/host')
+api.add_resource(HostListService, '/api/list/host')
 docs.register(HostListService)
 
 
-api.add_resource(Relation_UserHost_Service, '/relation/userhost/<userhost_id>')
+api.add_resource(Relation_UserHost_Service, '/api/relation/userhost/<userhost_id>')
 docs.register(Relation_UserHost_Service)
 
-api.add_resource(Relation_EventHost_Service, '/relation/eventhost/<eventhost_id>')
+api.add_resource(Relation_EventHost_Service, '/api/relation/eventhost/<eventhost_id>')
 docs.register(Relation_EventHost_Service)
 
-api.add_resource(Relation_EventTicket_Service, '/relation/eventticket/<eventticket_id>')
+api.add_resource(Relation_EventTicket_Service, '/api/relation/eventticket/<eventticket_id>')
 docs.register(Relation_EventTicket_Service)
 
-api.add_resource(CountService, '/count/<count_id>')
+api.add_resource(CountService, '/api/count/<count_id>')
 docs.register(CountService)
 
-api.add_resource(CountListService, '/list/count')
+api.add_resource(CountListService, '/api/list/count')
 docs.register(CountListService)
 
-api.add_resource(EventImageService, '/image/<event_id>')
+api.add_resource(EventImageService, '/api/image/<event_id>')
 docs.register(EventImageService)
 
-api.add_resource(CommentService, '/comment/<comment_id>')
+api.add_resource(CommentService, '/api/comment/<comment_id>')
 docs.register(CommentService)
-api.add_resource(CommentListService, '/list/comment/')
+api.add_resource(CommentListService, '/api/list/comment/')
 docs.register(CommentListService)
-api.add_resource(ListCommentsByEventService, '/list/comment/<event_id>')
+api.add_resource(ListCommentsByEventService, '/api/list/comment/<event_id>')
 docs.register(ListCommentsByEventService)
-api.add_resource(TicketCreateService, '/ticket/create/<event_id>/<ticket_id>')
+api.add_resource(TicketCreateService, '/api/ticket/create/<event_id>/<ticket_id>')
 docs.register(TicketCreateService)
-api.add_resource(TicketService, '/ticket/<ticket_id>')
+api.add_resource(TicketService, '/api/ticket/<ticket_id>')
 docs.register(TicketService)
 #*______________ Application Creation ______________
 if __name__ == '__main__':
